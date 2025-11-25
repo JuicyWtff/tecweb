e70cf3e614b3ca8d1641941bb954d8ec9f0c6019
JuicyWtff <hugobyjavier999@gmail.com>
2025-11-24 19:36:10 -0600

    Se actualizó product-single.php en el backend incluyendo autoload.

--- practicas/p13/product_app/backend/product-single.php
+++ practicas/p13/product_app/backend/product-single.php
@@ -1,41 +1,11 @@
 <?php
-    /*
-    include_once __DIR__.'/database.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array();
+    use MYAPI\Read\Products;
 
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = {$id}") ) {
-            // SE OBTIENEN LOS RESULTADOS
-            $row = $result->fetch_assoc();
-
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = utf8_encode($value);
-                }
-            }
-            $result->free();
-        } else {
-            die('Query Error: '.mysqli_error($conexion));
-        }
-        $conexion->close();
+    if (isset($_POST['id'])) {
+        $producto = new Products('marketzone');
+        $producto->single($_POST['id']);
+        echo $producto->getData();
     }
-
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
-     */
-
-    include_once __DIR__.'/myapi/Products.php';
-
-    $api = new \myapi\Products('marketzone');
-
-    if ( isset($_POST['id']) ) {
-        $api->single($_POST['id']);
-    }
-
-    echo $api->getData();
 ?>
