7b7bb3f9938769a9e0d1360d44223a5fade10446
JuicyWtff <hugobyjavier999@gmail.com>
2025-11-24 19:41:02 -0600

    Se actualizó product-delete.php en el backend incluyendo autoload.

--- practicas/p13/product_app/backend/product-delete.php
+++ practicas/p13/product_app/backend/product-delete.php
@@ -1,38 +1,11 @@
 <?php
-    /*
-     include_once __DIR__.'/database.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array(
-        'status'  => 'error',
-        'message' => 'La consulta falló'
-    );
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
-        if ( $conexion->query($sql) ) {
-            $data['status'] =  "success";
-            $data['message'] =  "Producto eliminado";
-		} else {
-            $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
-        }
-		$conexion->close();
-    } 
-    
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
-     */
+    use MYAPI\Delete\Products;
 
-    include_once __DIR__.'/myapi/Products.php';
-
-    $api = new \myapi\Products('marketzone');
-
-    if ( isset($_POST['id']) ) {
-        $api->delete($_POST['id']);
+    if (isset($_POST['id'])) {
+        $producto = new Products('marketzone');
+        $producto->delete($_POST['id']);
+        echo $producto->getData();
     }
-
-    echo $api->getData();
-
 ?>
