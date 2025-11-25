5c76d8b0d12057c00f656a8ba7dc8ae10ff33da0
JuicyWtff <hugobyjavier999@gmail.com>
2025-11-24 19:35:11 -0600

    Se actualizó product-search.php en el backend incluyendo autoload.

--- practicas/p13/product_app/backend/product-list.php
+++ practicas/p13/product_app/backend/product-list.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-use MYAPI\Read\Products;
+    use MYAPI\Read\Products;
 
-$productos = new Products('marketzone');
-$productos->list();
-echo $productos->getData();
+    $productos = new Products('marketzone');
+    $productos->list();
+    echo $productos->getData();
 ?>
